d3c6cf1fcf8e0cd0e97c52da89945f91ad056efb
liuwenjian <liuwenjian091@pingan.com.cn>
2019-09-11 13:39:05 +0800

    0.0.2 udpate

--- assets/plugin.js
+++ assets/plugin.js
@@ -1,25 +1,50 @@
-require(['gitbook'], function (gitbook) {
-  function addScript (url) {
+require(['gitbook'], function(gitbook) {
+  function addScript(url) {
     var script = document.createElement('script')
     script.src = url
     script.async = 1
-    var m = document.getElementsByTagName('head')[0]
+    var m = document.getElementsByTagName('script')[0]
     m.parentNode.insertBefore(script, m)
   }
-  gitbook.events.bind('start', function (e, config) {
+
+  function insertAfter(newNode, existNode) {
+    var parent = existNode.parentNode
+    if (parent.lastChild === existNode) {
+      parent.appendChild(newNode)
+    } else {
+      parent.insertBefore(newNode, existNode.nextSibling)
+    }
+  }
+
+  function addEvent(category, label, value) {
+    gtag('event', 'page', {
+      event_category: category,
+      event_label: label,
+      value: value
+    })
+  }
+
+  gitbook.events.bind('start', function(e, config) {
     var pluginConfig = config.gtag
     if (pluginConfig) {
-      addScript('https://www.googletagmanager.com/gtag/js?id=' + pluginConfig.token)
+      addScript(
+        'https://www.googletagmanager.com/gtag/js?id=' + pluginConfig.token
+      )
+    }
   })
 
-  gitbook.events.bind('page.change', function () {
-    var pluginsConfig = gitbook.state.config.pluginsConfig.gtag
+  gitbook.events.bind('page.change', function(e, config) {
+    var state = gitbook.state
+    var pluginsConfig = state.config.pluginsConfig.gtag
     if (pluginsConfig) {
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-  
-      gtag('config', config.token);
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+
+      gtag('config', pluginsConfig.token)
+      addEvent('navigate', 'menu', state.chapterTitle)
     }
   })
 })
